6d2dff3796c07c45f32c813353b0405e6f9aca8b
Seong1-Jo <seong12yada@gmail.com>
2023-01-25 21:50:06 +0900

    feat: First Git

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000; //포트 5000번 백서버
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jo:abcd1234@boilerplate01.rasmbsh.mongodb.net/?retryWrites=true&w=majority",
+    {}
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log("err", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! 안녕하세요");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
